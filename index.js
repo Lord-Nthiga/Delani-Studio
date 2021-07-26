5f0611d6941bec46f40a2a742cd947577feb010b
Lord-Nthiga <travis.nthiga@student.moringaschool.com>
2021-07-26 11:30:07 +0300

    modified js folder

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-var container2 = document.getElementById('container2')
-container2.style.right = '300px'
+$(document).ready(function(){
+    $(".img1").click(function() {
+        $(".img1").slideToggle(2000);
+        $(".design").slideToggle(2000);
+    });
+    $(".design").click(function() {
+        $(".design").slideToggle(2000);
+        $(".img1").slideToggle(2000);
+    });
+});
