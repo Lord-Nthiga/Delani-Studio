3611b9595b4c4a32c109c604b14dc1069d912331
Lord-Nthiga <travis.nthiga@student.moringaschool.com>
2021-08-06 04:13:07 +0200

    polishing project

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-$(document).ready(function(){
-    $(".img1").click(function() {
-        $(".img1").slideToggle(2000);
-        $(".design").slideToggle(2000);
-    });
-    $(".design").click(function() {
-        $(".design").slideToggle(2000);
-        $(".img1").slideToggle(2000);
-    });
-});
+$ function myFunction() {
+    var design = document.getElementById("design");
+    if (design.style.display === "none") {
+        design.style.display = "block";
+    } else {
+        design.style.display = "none";
+    }
+}
