e234213dcbd11c3b878990b6d27e2e2a2190aeeb
Lord-Nthiga <travis.nthiga@student.moringaschool.com>
2021-08-06 13:34:17 +0300

    fixed js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-$ function myFunction() {
-    var design = document.getElementById("design");
-    if (design.style.display === "none") {
-        design.style.display = "block";
-    } else {
-        design.style.display = "none";
-    }
-}
+$(".design") .click(function() {
+    $("#design") .show()
+})
