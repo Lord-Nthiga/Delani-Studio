600c5a4486fa37f913d96e3b80330152e663dbe5
Lord-Nthiga <travis.nthiga@student.moringaschool.com>
2021-08-06 14:45:26 +0300

    made changes to project

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+$(document).ready(function(){ 
+
 $(".design") .click(function() {
     $("#design") .show()
+    $(".design") .hide()
+})
+$("#design") .click(function() {
+    $(".design") .show()
+    $("#design") .hide()
+})
+
+$(".work1img").hover(function() {
+    $("h4").show()
+})
+
 })
